1304b191a47fb99f87e6fde13bb11f69b6c6fd98
Christofer Flores <cristofer.flort@gmail.com>
2019-08-25 15:50:24 -0500

    Created the todo component

---
+++ ruby_react/app/javascript/components/FancyWords.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react"
+
+function FancyWords(props) {
+
+  const [ task, updateTask ] = useState("")
+  const [ todos, setTodos ] = useState([])
+
+  function addTodo(newTask) {
+    if (todos.length < props.todosCount) {
+      const allTodos = [...todos, newTask]
+      setTodos(allTodos)
+      updateTask("")
+      return
+    }
+
+    alert('Cant create more than ' + props.todosCount + 'todos for this list')
+  }
+
+  return (
+    <>
+      <div className="word-container">
+        <p>You still can add {props.todosCount - todos.length} of {props.todosCount}</p>
+        <input
+          type="text"
+          value={task}
+          autoFocus
+          onChange={(e) => updateTask(e.target.value)}
+        />
+        <button onClick={() => addTodo(task)}>Add</button>
+        <hr />
+        <ul>
+          {todos.map((t, i) => (<li key={t+i}>{t}</li>))}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default FancyWords
